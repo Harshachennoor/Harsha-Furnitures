dd1db78036b2e8bc6000d5e74699fcfb5db2a3d5
harshach <harshachennoor@gmail.com>
2023-12-20 09:05:19 -0600

    Fourth Commit

--- src/components/NavBar/CategoryItems.jsx
+++ src/components/NavBar/CategoryItems.jsx
@@ -1,27 +1,34 @@
 import React, { useState } from "react";
 import CategoryItemOverview from "./CategoryItemOverview";
 
-
 function CategoryItems(props) {
 
     //Hook used to expand the Category Items and set currentCategory
     const [cursorOnLi,setExpandDiv] = useState(false);
     const [currentCategory, setCurrentCategory] = useState("");
+    const [isHover, setHover]=useState(false);
+
+    const style = {
+        borderBottom: `2px solid ${isHover ? 'blue' : 'transparent'}`, // Change color on hover
+        
+    };
 
     // Function is called when mouse is Over the <li> tag
     function handleMouseEnter(event){
+        setHover(true);
         setExpandDiv(true);
         setCurrentCategory(event.currentTarget.getAttribute('category-name'));
     }
 
     // Function is called when mouse leaves the <li> tag
     function handleMouseLeave(){
+        setHover(false);
         setExpandDiv(false);
     }
 
     return (
         <li onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} category-name={props.Category} className="item" >
-            <a href="https://www.google.com/" value={props.Category}>{props.Category}</a>{/* Example: Mattress */}
+            <a style={style} href="https://www.google.com/" value={props.Category}>{props.Category}</a>{/* Example: Mattress */}
             {/* Conditional Rendering */}
             {cursorOnLi && <CategoryItemOverview 
             target={currentCategory}/>}
